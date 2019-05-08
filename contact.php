cbab17f368a69da43e3bda5c20286dd64352ce12
Arpit Das <arpitdas118@gmail.com>
2019-05-09 01:17:37 +0530

    First commit

---
+++ contact.php
@@ -0,0 +1,20 @@
+<?php
+$name = $_POST['Name'];
+$email = $_POST['Email'];
+$msg = $_POST['Message'];
+
+/*Connecting to Database*/
+$ip = "localhost";
+$user = "root";
+$pass = "";
+$dbname = "NSEC";
+/*END*/
+
+$connect = mysqli_connect($ip,$user,$pass,$dbname);
+
+$insert = "INSERT INTO Contact SET Name = '$name', Email = '$email', Message = '$msg'";
+
+$connect->query($insert);
+
+header('location:index.html')
+?>
---
+++ js/index.js
@@ -0,0 +1,41 @@
+
+function w3_open() {
+  document.getElementById("mySidebar").style.display = "block";
+}
+
+function w3_close() {
+  document.getElementById("mySidebar").style.display = "none";
+}
+
+function w3_open_contct1(){
+	document.getElementById('menu1').style.display = "block";
+}
+
+function w3_close_contct1(){
+	document.getElementById('menu1').style.display = "none";
+}
+
+function w3_open_contct2(){
+	document.getElementById('menu2').style.display = "block";
+}
+
+function w3_close_contct2(){
+	document.getElementById('menu2').style.display = "none";
+}
+
+function w3_open_contct3(){
+	document.getElementById('menu3').style.display = "block";
+}
+
+function w3_close_contct3(){
+	document.getElementById('menu3').style.display = "none";
+}
+
+function w3_open_contct4(){
+	document.getElementById('menu4').style.display = "block";
+}
+
+function w3_close_contct4(){
+	document.getElementById('menu4').style.display = "none";
+}
+
