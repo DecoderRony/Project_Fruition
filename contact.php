fb88879ab3dbbb2ebd9edb47cc66f40608f385dd
Ryan Das <ryandas@protonmail.com>
2019-05-11 14:38:10 +0530

    user query service working

--- contact.php
+++ contact.php
@@ -7,12 +7,16 @@ $subject = "Query by User";
 $header = "From: $email";
 /*Connecting to Database*/
 $ip = "localhost";
-$user = "root";
-$pass = "";
-$dbname = "NSEC";
+$user = "id1802919_freefolk";
+$pass = "freak";
+$dbname = "id1802919_nsec";
 /*END*/
 
 $connect = mysqli_connect($ip,$user,$pass,$dbname);
+
+if(mysqli_connect_errno()){
+    die("Connection error"."\r\n".mysqli_connect_error());
+}
 
 $insert = "INSERT INTO Contact SET Name = '$name', Email = '$email', Message = '$msg'";
 
